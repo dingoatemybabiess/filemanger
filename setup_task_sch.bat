b8bce2b7242080071311f3b2ba130ec0d71c20eb
dingoatemybabiesss <ghoniemohamed@gmail.com>
2025-10-03 23:49:37 +0300

    updated

--- setup_task_sch.bat
+++ setup_task_sch.bat
@@ -1,6 +1,15 @@
 @echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~f0' -Verb runAs"
+    exit /b
+)
+
 REM Creates a Task Scheduler task to run run_hidden.vbs on user logon
 
-schtasks /Create /TN "FileManagerMonitor" /TR "\"%~dp0run_invs.vbs\"" /SC ONLOGON /RL HIGHEST /F
+schtasks /Create /TN "FileManager" /TR "\"%~dpx0run_invs.vbs\"" /SC ONSTART /RL HIGHEST /F
 
 echo Task Scheduler entry created.
+pause
